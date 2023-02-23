684a0fd0a52d1df25fc81cf4e2fa6213fa7e9a32
Your Name <olmecadex@gmail.com>
2023-02-23 17:16:27 -0600

    silly tokenizer

--- Research/tokenizer/Production-Code/tokenizer.cpp
+++ Research/tokenizer/Production-Code/tokenizer.cpp
@@ -1,26 +1,9 @@
 #include <boost/tokenizer.hpp>
-#include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 
-std::vector<std::string> tokenize(const std::string& text) {
-    // Create a tokenizer that splits on whitespace and punctuation
-    boost::char_separator<char> separator("", " \t\n\r\".,:;()[]{}!?");
-    boost::tokenizer<boost::char_separator<char>> tokens(text, separator);
-
-    // Convert the tokens to a vector of strings
-    std::vector<std::string> result(tokens.begin(), tokens.end());
-
-    return result;
-}
-
-int main() {
-    std::string text = "The quick brown fox jumps over the lazy dog.";
-    std::vector<std::string> tokens = tokenize(text);
-
-    for (const auto& token : tokens) {
-        std::cout << token << std::endl;
-    }
-
-    return 0;
+std::vector<std::string> splitString(const std::string& text, const char* separator = " ") {
+    boost::char_separator<char> sep(separator);
+    boost::tokenizer<boost::char_separator<char>> tokens(text, sep);
+    return std::vector<std::string>(tokens.begin(), tokens.end());
 }
