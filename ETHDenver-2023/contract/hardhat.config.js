4e17b767bce04ebf5ca06ef75418054d7f79f377
Your Name <olmecadex@gmail.com>
2023-03-04 13:11:07 -0600

    update

--- ETHDenver-2023/contract/hardhat.config.js
+++ ETHDenver-2023/contract/hardhat.config.js
@@ -1,21 +1,14 @@
-/** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
+  solidity: "0.8.0",
   networks: {
     hardhat: {},
-    ganache: {
-      url: "http://127.0.0.1:8545",
-      chainId: 1337
-    }
   },
-  solidity: {
-    version: "0.8.0",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
+  etherscan: {
+    apiKey: "YOUR_ETHERSCAN_API_KEY",
   },
 };
+
