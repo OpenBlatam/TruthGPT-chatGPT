059292d85183c930cd6d5ddf0dce917b0f547cac
Your Name <olmecadex@gmail.com>
2023-03-05 03:26:04 -0600

    clean up on fly

--- ETHDenver-2023/ETHSH-hackathon-back-main 2/apollo-client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-    uri: "https://api.thegraph.com/subgraphs/name/pierregvx/uma",
-    cache: new InMemoryCache(),
-});
-
-export default client;
